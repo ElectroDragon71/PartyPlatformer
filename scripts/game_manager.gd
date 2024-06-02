c81b7e9503e1fd6a8d55733a4e75de59a2c373fe
Jennie Li <jcl2044@rit.edu>
2024-06-02 00:01:55 -0400

    Started work on Multiplayer
    Started Implementing multiplayer

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,3 +5,13 @@ var score = 0
 func add_point():
 	score += 1
 	print(score)
+
+func become_host():
+	print("Become Host Pressed")
+	%MultiplayerHUD.hide()
+	MultiplayerManager.become_host()
+
+func join_as_player_2():
+	print("Joined as Player 2")
+	%MultiplayerHUD.hide()
+	MultiplayerManager.join_as_player_2()
