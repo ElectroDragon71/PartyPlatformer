f3cc76a5e0badb5d3e8808ca8c27caa024608f69
Jennie Li <jcl2044@rit.edu>
2024-06-22 14:09:27 -0400

    Fixing Multiplayer Placement
    Server will spawn the object, then give control over to the client

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,6 +1,7 @@
 class_name Inventory extends Popup
 
 @export var item_placer: ItemPlacer
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
---
+++ scripts/multiplayer_item_spawner.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func spawn_item():
+	pass
