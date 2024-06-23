5a058b801aa875ad24131cebe367e3613c79e042
Jennie Li <jcl2044@rit.edu>
2024-06-23 13:15:06 -0400

    Working on multiplayer placement

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -2,6 +2,9 @@ extends Node
 
 var score = 0
 
+func _ready():
+	NodePaths.current_level = "LevelMenu"
+
 func add_point():
 	score += 1
 	print(score)
--- scripts/node_paths.gd
+++ scripts/node_paths.gd
@@ -1,11 +1,6 @@
 extends Node
 
+var current_level
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+func get_level():
+	get_tree().root.get_node(current_level)
